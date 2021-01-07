3aba1ae02a24a5f0986ba082737706e1e62f0d11
Tatyana <->
2021-01-07 19:34:41 +0300

    [dev] now the navigation works

---
+++ docs/index.md
@@ -0,0 +1,5 @@
+---
+sidebar_label: Index
+title: Index
+slug: /
+---
--- sidebars.js
+++ sidebars.js
@@ -1,5 +1,8 @@
 module.exports = {
     docs: [{
+        type: "doc",
+        id: "index",
+    }, {
         type: "category",
         label: "Pagination",
         collapsed: true,
